96694b31ccc35b6238506de217bfc1c40c2dfa08
Abdul <2024-cse-035@uvas.edu.pk>
2026-08-30 23:31:20 +0500

    errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,10 @@ plugins {
 
 android {
     namespace = "com.example.wisp"
-    compileSdk = flutter.compileSdkVersion
+    
+    // Updated to 36 to satisfy plugin dependencies
+    compileSdk = 36 
+    
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -15,20 +18,17 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.wisp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        
+        minSdk = 24
+        targetSdk = 34 // targetSdk can remain 34 or be set to 36
+        
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,20 +5,32 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Align subproject plugin compile SDKs with SDK 36
+subprojects {
+    plugins.withId("com.android.library") {
+        (extensions.findByName("android") as? com.android.build.gradle.BaseExtension)?.compileSdkVersion(36)
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+subprojects {
+    afterEvaluate {
+        val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        android?.compileSdkVersion(36)
+    }
 }
